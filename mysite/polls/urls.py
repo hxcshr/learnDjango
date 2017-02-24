ac828fad46238effdb2a114c9428ffde815ed657
hxc <hexuec@babycan.cc>
2017-02-24 10:27:05 +0800

    测试完成

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url 
 from . import views
 
-urlpatterns = [url(r'^$',views.index,name='index'),
-               url(r'^(?P<question_id>[0-9]+)/$',views.detail,name='detail'),
-               url(r'^(?P<question_id>[0-9]+)/results/$',views.results,name='results'),
+app_name = 'polls'
+urlpatterns = [url(r'^$',views.IndexView.as_view(),name='index'),
+               url(r'^(?P<pk>[0-9]+)/$',views.DetailView.as_view(),name='detail'),
+               url(r'^(?P<pk>[0-9]+)/results/$',views.ResultsView.as_view(),name='results'),
                # ex: /polls/5/vote/
                url(r'^(?P<question_id>[0-9]+)/vote/$',views.vote,name='vote')
                ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,12 +1,32 @@
 from django.shortcuts import render,get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.http import Http404
+from django.views import generic
+from django.urls import reverse
 
 from django.template import loader
 
-from .models import Question
+from .models import Question,Choice
 
 # Create your views here.
+
+class IndexView(generic.ListView):
+        template_name = 'polls/index.html'
+        context_object_name = 'latest_question_list'
+
+        def get_queryset(self):
+            return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+        model = Question
+        template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+        model = Question
+        template_name = 'polls/results.html'
+
 
 def index(request):
         latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -26,8 +46,21 @@ def detail(request,question_id):
 
 
 def results(request,question_id):
-        response = "you're looking at the results of question %s."
-        return HttpResponse(response % question_id)
+        question = get_object_or_404(Question,pk=question_id)
+        return render(request,'polls/results.html',{'question': question})
 
 def vote(request, question_id):
-        return HttpResponse("you're voting on question %s." % question_id)
+        question = get_object_or_404(Question,pk=question_id)
+        try:
+            selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        except (KeyError, Choice.DoesNotExist):
+            #redisplay the question voting form.
+            return render(request,'polls/detail.html',{
+                          'question':question,
+                          'error_message':"You didn't select a choice."
+                          })
+        else:
+            selected_choice.votes += 1
+            selected_choice.save()
+            return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
+
